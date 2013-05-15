42f4964d49daef586515d1f0596090496f8c6270
bmprovan <bmprovan@gmail.com>
2013-05-14 20:01:08 -0500

    Fixed event loop only processing one event at a time.

--- ToastFramework/ToastEventsManager.cpp
+++ ToastFramework/ToastEventsManager.cpp
@@ -25,14 +25,6 @@ namespace Toast
 
 	void EventManager::checkEvents()
 	{
-		ALLEGRO_EVENT ev;
-		ALLEGRO_TIMEOUT timeout;
-		// it waits only a millisecond for an incoming event
-		al_init_timeout(&timeout, 0.001);
- 
-		bool get_event = al_wait_for_event_until(event_queue, &ev, &timeout);
-		//al_wait_for_event(event_queue, &ev);
- 
 		// resetting frame based input stuff
 		Input::leftMousePressed=false;
 		for(unsigned int i=0; i < TOAST_KEY_MAX; i++)
@@ -40,43 +32,50 @@ namespace Toast
 			Input::pressedKeys[i] = false;
 		}
 
+		//If any events happen, process all of them this frame.
+		while(!al_is_event_queue_empty(event_queue))
+		{	
+			ALLEGRO_EVENT ev;
 
-		if(get_event && ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) 
-		{
-			this->onExit();
-		}
-		else if(ev.type == ALLEGRO_EVENT_MOUSE_AXES ||
-				ev.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY)
-		{
-			Input::mouseWindowX = ev.mouse.x;
-			Input::mouseWindowY = ev.mouse.y;
+			al_wait_for_event(event_queue, &ev);
 
-			Input::mouseX = Input::mouseWindowX * Input::mouseScale;
-			Input::mouseY = (Input::mouseWindowY * Input::mouseScale) - (Input::offsetY * Input::mouseScale);
-			
-		}
-		else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
-		{
-			if(ev.mouse.button == 1)
+			if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) 
 			{
-				Input::leftMousePressed=true;
-				Input::leftMouseDown=true;
-				Input::mouseReleased=false;
+				this->onExit();
 			}
-		}
-		else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
-		{
-			Input::leftMouseDown=false;
-			Input::mouseReleased=true;
-		}
-		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
-		{
-			Input::pressedKeys[ev.keyboard.keycode] = true;
-			Input::downKeys[ev.keyboard.keycode] = true;
-		}
-		else if(ev.type == ALLEGRO_EVENT_KEY_UP)
-		{
-			Input::downKeys[ev.keyboard.keycode] = false;
+			else if(ev.type == ALLEGRO_EVENT_MOUSE_AXES ||
+				ev.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY)
+			{
+				Input::mouseWindowX = ev.mouse.x;
+				Input::mouseWindowY = ev.mouse.y;
+
+				Input::mouseX = Input::mouseWindowX * Input::mouseScale;
+				Input::mouseY = (Input::mouseWindowY * Input::mouseScale) - (Input::offsetY * Input::mouseScale);
+
+			}
+			else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
+			{
+				if(ev.mouse.button == 1)
+				{
+					Input::leftMousePressed=true;
+					Input::leftMouseDown=true;
+					Input::mouseReleased=false;
+				}
+			}
+			else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
+			{
+				Input::leftMouseDown=false;
+				Input::mouseReleased=true;
+			}
+			else if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
+			{
+				Input::pressedKeys[ev.keyboard.keycode] = true;
+				Input::downKeys[ev.keyboard.keycode] = true;
+			}
+			else if(ev.type == ALLEGRO_EVENT_KEY_UP)
+			{
+				Input::downKeys[ev.keyboard.keycode] = false;
+			}
 		}
 	}
 }
